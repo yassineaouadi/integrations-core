3840eeaf8ea786fa2f2f5d997b3125a666bf46f5
Justin Iso <justin.james.iso@gmail.com>
2020-10-20 09:23:06 -0400

    Move warning log to debug

--- datadog_checks_base/datadog_checks/base/utils/db/statement_metrics.py
+++ datadog_checks_base/datadog_checks/base/utils/db/statement_metrics.py
@@ -46,7 +46,7 @@ class StatementMetrics:
         for row in rows:
             row_key = key(row)
             if row_key in new_cache:
-                self.log.warning(
+                self.log.debug(
                     'Collision in cached query metrics. Dropping existing row, row_key=%s new=%s dropped=%s',
                     row_key,
                     row,
